e91165e9af029073bd301a0644168860e72ebdcf
Arihant Jain <81769948+arihantjain916@users.noreply.github.com>
2021-07-11 19:29:32 +0530

    Add files via upload

--- tutor/settings.py
+++ tutor/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-28w=f*=w-_8&d)5nxlub3-ahuy*1qd4snnmwbc82cygyy!%p8i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["great-minded-tutors-academy.herokuapp.com" , "127.0.0.1"]
 
@@ -41,8 +43,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
+
+
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,8 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR , "static")
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR , "static")
+# STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -129,6 +138,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Added Manually
 # STATICFILES_DIRS = [
-#     BASE_DIR / "static",
+#     BASE_DIR  / "static",
 # ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
 
